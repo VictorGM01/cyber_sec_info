480a1c98c2aa8a9b064efaedd97b563d20971753
VictorGM01 <victormarques8801@gmail.com>
2022-04-22 18:02:09 -0300

    Configurando a relação de views.py com urls.py

--- app_cyber_sec/urls.py
+++ app_cyber_sec/urls.py
@@ -1,3 +1,6 @@
 from django.urls import path
+from . import views
 
-urlpatterns = []
+urlpatterns = [
+    path('', views.index, name='home-page')
+]
--- app_cyber_sec/views.py
+++ app_cyber_sec/views.py
@@ -1,3 +1,5 @@
-from django.shortcuts import render
+from django.shortcuts import render, HttpResponse
 
-# Create your views here.
+
+def index(request):
+    return HttpResponse("<title>Cyber Sec. Info</title>")  # Apenas para teste
