bf7dc4abc6e6714015e516029ac6510a156b39a0
Raphaela Ferraz <86068799+raphaelaferraz@users.noreply.github.com>
2022-05-15 19:17:39 -0300

    Outras funcionalidades do cabeçalho (ícones)

--- project_cyber_sec/static/assets/js/cabecalho.js
+++ project_cyber_sec/static/assets/js/cabecalho.js
@@ -6,19 +6,23 @@ $('#cabecalho_menu_checkbox').on('click', function(){
 // Abertura do cabeçalho mobile - Sobre nós
 $('#cabecalho_navegacao_item_sobre_nos').on('click', function() {
   $('#cabecalho_navegacao_sublista_sobre_nos').slideToggle('slow').css('display', 'block');
+  $(this).toggleClass('rodar__item__link--icone');
 });
 
 // Abertura do cabeçalho mobile - Ferramentas
 $('#cabecalho_navegacao_item_ferramentas').on('click', function() {
   $('#cabecalho_navegacao_sublista_ferramentas').slideToggle('slow').css('display', 'block');
+  $(this).toggleClass('rodar__item__link--icone');
 });
 
 // Abertura do cabeçalho mobile - Tutoriais
 $('#cabecalho_navegacao_item_tutoriais').on('click', function() {
   $('#cabecalho_navegacao_sublista_tutoriais').slideToggle('slow').css('display', 'block');
+  $(this).toggleClass('rodar__item__link--icone');
 });
 
 // Abertura do cabeçalho mobile - Conceitos
 $('#cabecalho_navegacao_item_conceitos').on('click', function() {
   $('#cabecalho_navegacao_sublista_conceitos').slideToggle('slow').css('display', 'block');
+  $(this).toggleClass('rodar__item__link--icone');
 });
