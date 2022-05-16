683c56ff2ab4556e0cd94fab4c596bc7bbcf153f
Victor G. Marques <victormarques8801@gmail.com>
2022-05-15 21:20:09 -0300

    Alteração para redirecionamento da pág fale-conosco

--- apps/emails_cyber_sec/urls.py
+++ apps/emails_cyber_sec/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('/fale-conosco', views.fale_conosco, name='fale-conosco')
+    path('fale-conosco', views.fale_conosco, name='fale-conosco')
 ]
--- apps/emails_cyber_sec/views.py
+++ apps/emails_cyber_sec/views.py
@@ -1,3 +1,5 @@
-from django.shortcuts import render
+from django.shortcuts import render, HttpResponse
 
-# Create your views here.
+
+def fale_conosco(request):
+    return HttpResponse("<h1>Fale Conosco</h1>")  # Apenas para teste
