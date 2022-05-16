55b9399457e204a7a964a91143b284c3d57416e9
Victor G. Marques <victormarques8801@gmail.com>
2022-05-15 21:05:22 -0300

    Cria modelo para cadastro de e-mails para newsletter

--- apps/emails_cyber_sec/models.py
+++ apps/emails_cyber_sec/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+
+# Modelo para usuários que receberão os e-mails da newsletter
+class Email(models.Model):
+    nome = models.CharField(max_length=40)
+    email = models.EmailField()
+
+    def __str__(self):
+        return self.email
