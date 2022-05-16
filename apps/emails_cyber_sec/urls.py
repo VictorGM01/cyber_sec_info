e42c923435e4c5ca53cbb8b8e78944fde174df91
Victor G. Marques <victormarques8801@gmail.com>
2022-05-15 21:56:44 -0300

    Configurações básicas para página de contato (fale conosco)

--- apps/emails_cyber_sec/urls.py
+++ apps/emails_cyber_sec/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('fale-conosco', views.fale_conosco, name='fale-conosco')
+    path('fale-conosco', views.fale_conosco, name='fale-conosco'),
+    path('receber-conteudos', views.receber_conteudos, name='receber-conteudos')
 ]
--- apps/emails_cyber_sec/views.py
+++ apps/emails_cyber_sec/views.py
@@ -3,3 +3,7 @@ from django.shortcuts import render, HttpResponse
 
 def fale_conosco(request):
     return HttpResponse("<h1>Fale Conosco</h1>")  # Apenas para teste
+
+
+def receber_conteudos(request):
+    return HttpResponse("<h1>Deseja receber e-mails sobre nossas publicações?</h1>")  # Apenas para teste
