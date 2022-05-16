4e3ad4b8447c49668de0a312cc04a65a094f6466
Victor G. Marques <victormarques8801@gmail.com>
2022-05-15 22:28:26 -0300

    Criação do método para envio dos e-mails

--- apps/emails_cyber_sec/views.py
+++ apps/emails_cyber_sec/views.py
@@ -1,4 +1,9 @@
 from django.shortcuts import render, HttpResponse
+from django.core.mail import EmailMultiAlternatives
+from django.template.loader import render_to_string
+from project_cyber_sec import settings
+
+from .models import Email
 
 
 def fale_conosco(request):
@@ -7,3 +12,19 @@ def fale_conosco(request):
 
 def receber_conteudos(request):
     return HttpResponse("<h1>Deseja receber e-mails sobre nossas publicações?</h1>")  # Apenas para teste
+
+
+def envia_email(request, nome, destino, assunto_artigo, categoria, resumo):
+    assunto = "Cyber Security Information: Novo Conteúdo"  # Assunto do e-mail
+    mensagem = "Atualização"
+    email_host = settings.EMAIL_HOST_USER  # E-mail que enviará a mensagem
+
+    email_destino = [destino]  # Destinatário
+
+    email = EmailMultiAlternatives(assunto, mensagem, email_host, email_destino)
+
+    msg = render_to_string("emails/envio_de_email.html", context={
+        "nome": nome, "assunto_artigo": assunto_artigo, "categoria": categoria, "resumo": resumo})  # Mensagem em html
+
+    email.attach_alternative(msg, "text/html")
+    email.send()
--- project_cyber_sec/settings.py
+++ project_cyber_sec/settings.py
@@ -136,3 +136,10 @@ MEDIA_URL = '/media/'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
+EMAIL_HOST = "smtp.gmail.com"
+EMAIL_USE_TLS = True
+EMAIL_PORT = 587
+EMAIL_HOST_USER = "grupopji2021@gmail.com"
+EMAIL_HOST_PASSWORD = config('PASS')
