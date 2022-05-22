86344a91edcfe0ea8d356401aad960091a24dfa1
Victor G. Marques <victormarques8801@gmail.com>
2022-05-22 12:13:59 -0300

    CORREÇÃO: Middlewares

--- project_cyber_sec/settings.py
+++ project_cyber_sec/settings.py
@@ -47,7 +47,7 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
-    'django.middleware.security.SecurityMiddleware'
+    'django.middleware.security.SecurityMiddleware',
     'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
