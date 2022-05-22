a91ab515e35b32514446f348b6f55435c41d7093
Victor G. Marques <victormarques8801@gmail.com>
2022-05-22 12:07:27 -0300

    Configuração para deploy: Whitenoise e Procfile

--- Procfile
+++ Procfile
@@ -1,2 +1,2 @@
 release: python manage.py migrate
-web: gunicorn project_cyber_sec.wsgi
+web: gunicorn project_cyber_sec.wsgi --preload --log-file -
--- project_cyber_sec/settings.py
+++ project_cyber_sec/settings.py
@@ -47,14 +47,14 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
-    'django.middleware.security.SecurityMiddleware',
+    'django.middleware.security.SecurityMiddleware'
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
 ROOT_URLCONF = 'project_cyber_sec.urls'
