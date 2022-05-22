7785a6e2869476da80d3f8ecae442aeb2335d494
Victor G. Marques <victormarques8801@gmail.com>
2022-05-21 22:09:05 -0300

    Configuração para deploy: settings

--- project_cyber_sec/settings.py
+++ project_cyber_sec/settings.py
@@ -116,6 +116,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
+DISABLE_COLLECTSTATIC = 1
+
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 STATICFILES_DIRS = [
