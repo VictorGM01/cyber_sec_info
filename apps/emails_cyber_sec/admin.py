19bae83fd8ee2cf308d9155512265893ef146579
Victor G. Marques <victormarques8801@gmail.com>
2022-05-22 16:30:10 -0300

    Cadastro bd (newsletter): Personalização do admin

--- apps/emails_cyber_sec/admin.py
+++ apps/emails_cyber_sec/admin.py
@@ -1,5 +1,12 @@
 from django.contrib import admin
 from .models import Email
 
-# Register your models here.
-admin.site.register(Email)
+
+class ListaEmails(admin.ModelAdmin):
+    list_display = ('id', 'email', 'nome')
+    list_display_links = ('id', 'email')
+    search_fields = ('email',)
+    list_per_page = 20
+
+
+admin.site.register(Email, ListaEmails)
