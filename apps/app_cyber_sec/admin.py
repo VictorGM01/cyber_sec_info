a79bb195b951d66750d01a672eac2fbb7ae10977
Victor G. Marques <victormarques8801@gmail.com>
2022-05-29 20:58:19 -0300

    Config. da exibição do modelo de artigos no admin

--- apps/app_cyber_sec/admin.py
+++ apps/app_cyber_sec/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
+from .models import Artigo
 
-# Register your models here.
+
+class ListaArtigos(admin.ModelAdmin):
+    list_display = ('id', 'titulo_para_caminho', 'data_publicacao', 'publicado')
+    list_display_links = ('id', 'titulo_para_caminho')
+    list_editable = ('publicado', )
+    search_fields = ('titulo_para_caminho', 'titulo')
+    list_per_page = 10
+
+
+admin.site.register(Artigo, ListaArtigos)
