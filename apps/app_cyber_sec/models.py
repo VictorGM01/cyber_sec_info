b279c46f342a27bb92a2d659bda1100f0017a941
Victor G. Marques <victormarques8801@gmail.com>
2022-05-29 20:37:28 -0300

    Criação do modelo do bd para os artigos

--- apps/app_cyber_sec/models.py
+++ apps/app_cyber_sec/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+from datetime import datetime
 
-# Create your models here.
+
+# Modelo para os artigos
+class Artigo(models.Model):
+    titulo = models.CharField(max_length=100)
+    titulo_para_caminho = models.CharField(max_length=30)
+    capa = models.ImageField(upload_to='artigos/%Y/%m/%d/')
+    data_publicacao = models.DateField(default=datetime.today)
+    tempo_de_leitura = models.IntegerField()
+    conteudo = models.TextField()
+    publicado = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.titulo_para_caminho
