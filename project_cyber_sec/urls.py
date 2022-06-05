679b7b4cabc97a3fae63091f87c3a88866b29e16
Victor G. Marques <victormarques8801@gmail.com>
2022-06-05 14:13:33 -0300

    Configuração para exibição de mídias

--- project_cyber_sec/urls.py
+++ project_cyber_sec/urls.py
@@ -15,10 +15,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include("apps.principal_cyber_sec.urls")),
     path('email/', include("apps.emails_cyber_sec.urls")),
     path('artigos/', include("apps.artigos_cyber_sec.urls")),
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
