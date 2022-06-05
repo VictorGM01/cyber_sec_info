5ae5680ddc86aabe56279c3d4fb30e9bea03c876
Victor G. Marques <victormarques8801@gmail.com>
2022-06-05 13:46:10 -0300

    Configurações da view e da url para detalhamento dos artigos

--- apps/artigos_cyber_sec/urls.py
+++ apps/artigos_cyber_sec/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 from . import views
-from django.conf import settings
 
 urlpatterns = [
+    path('<int:id_artigo>', views.artigo, name='artigo'),
 ]
--- apps/artigos_cyber_sec/views.py
+++ apps/artigos_cyber_sec/views.py
@@ -1,1 +1,14 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from .models import Artigo
+
+
+def artigo(request, id_artigo):
+
+    artigo_a_ser_exibido = get_object_or_404(Artigo, pk=id_artigo)
+
+    conteudo = {
+        'artigo': artigo_a_ser_exibido
+    }
+
+    return render(request, 'artigos/artigo.html', context=conteudo)
+
