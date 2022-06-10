233f39c39474273259c131f97e340ff2cd45e330
Victor G. Marques <victormarques8801@gmail.com>
2022-06-09 21:21:56 -0300

    Adição da url de compartilhamento via facebook

--- apps/artigos_cyber_sec/views.py
+++ apps/artigos_cyber_sec/views.py
@@ -7,7 +7,8 @@ def artigo(request, id_artigo):
     artigo_a_ser_exibido = get_object_or_404(Artigo, pk=id_artigo)
 
     conteudo = {
-        'artigo': artigo_a_ser_exibido
+        'artigo': artigo_a_ser_exibido,
+        'request': request,
     }
 
     return render(request, 'artigos/artigo.html', context=conteudo)
