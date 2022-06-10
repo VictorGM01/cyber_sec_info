e0f1636862c23b3d6eae609d2b1dc96391036186
Victor G. Marques <victormarques8801@gmail.com>
2022-06-09 22:16:57 -0300

    Mudança na configuração da uri

--- apps/artigos_cyber_sec/urls.py
+++ apps/artigos_cyber_sec/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('<int:id_artigo>', views.artigo, name='artigo'),
+    path('<str:nome_artigo>', views.artigo, name='artigo'),
 ]
--- apps/artigos_cyber_sec/views.py
+++ apps/artigos_cyber_sec/views.py
@@ -2,9 +2,9 @@ from django.shortcuts import render, get_object_or_404
 from .models import Artigo
 
 
-def artigo(request, id_artigo):
+def artigo(request, nome_artigo):
 
-    artigo_a_ser_exibido = get_object_or_404(Artigo, pk=id_artigo)
+    artigo_a_ser_exibido = get_object_or_404(Artigo, titulo_para_caminho=nome_artigo)
 
     conteudo = {
         'artigo': artigo_a_ser_exibido,
