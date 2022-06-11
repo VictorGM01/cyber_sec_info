935ea44a7264fb9d8320cba69e2b37efda46c389
Victor G. Marques <victormarques8801@gmail.com>
2022-06-10 22:53:15 -0300

    Url, view, padronização e implementação da página de devs

--- apps/principal_cyber_sec/urls.py
+++ apps/principal_cyber_sec/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='home-page')
+    path('', views.index, name='home-page'),
+    path('desenvolvedores', views.desenvolvedores, name='desenvolvedores'),
 ]
--- apps/principal_cyber_sec/views.py
+++ apps/principal_cyber_sec/views.py
@@ -4,3 +4,7 @@ from django.shortcuts import render
 def index(request):
     return render(request, "index.html")
 
+
+def desenvolvedores(request):
+    return render(request, "principal/desenvolvedores.html")
+
