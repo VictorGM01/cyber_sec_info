c0832441326d99f6f74f536d091af821f7bc6ef6
Victor G. Marques <victormarques8801@gmail.com>
2022-07-20 10:59:43 -0300

    Adição de novas validações nas páginas dos artigos

--- apps/artigos_cyber_sec/views.py
+++ apps/artigos_cyber_sec/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
 from django.views.decorators.cache import cache_page
 from .models import Artigo
 from django.core.paginator import Paginator
@@ -7,8 +7,8 @@ from django.db.models import Q
 
 def artigos(request):
     busca = request.GET.get('busca')
-    artigos = Artigo.objects.order_by('-data_publicacao').filter(publicado=True)
-    artigos_recentes = artigos[:3]
+    artigos = Artigo.objects.order_by('data_publicacao').filter(publicado=True)
+    artigos_recentes = Artigo.objects.order_by('-data_publicacao').filter(publicado=True)
 
     paginator = Paginator(artigos, 9)
 
@@ -19,7 +19,7 @@ def artigos(request):
     artigos_por_pagina = paginator.get_page(pagina)
 
     if busca:
-        artigos_buscados = Artigo.objects.filter(Q(titulo__icontains=busca) | Q(conteudo__icontains=busca))
+        artigos_buscados = Artigo.objects.filter(Q(titulo__icontains=busca) | Q(conteudo__icontains=busca)).filter(publicado=True)
         return render(request, 'artigos/geral_artigos.html', context={'artigos': artigos_buscados, 'busca': True})
 
     conteudo = {
@@ -35,6 +35,9 @@ def artigos(request):
 def exibe_artigo(request, nome_artigo):
 
     artigo_a_ser_exibido = get_object_or_404(Artigo, titulo_para_caminho=nome_artigo)
+
+    if not artigo_a_ser_exibido.publicado:
+        return redirect('artigos')
 
     artigos_relacionados = Artigo.objects.order_by('-data_publicacao').filter(publicado=True).filter(
         tema__icontains=artigo_a_ser_exibido.tema).exclude(titulo_para_caminho=nome_artigo)[:2]
