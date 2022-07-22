74d057b1f7db1bc07a232f96dbbf5be9d60209ca
Victor G. Marques <victormarques8801@gmail.com>
2022-07-22 18:14:59 -0300

    Adição da tabela de tutoriais no admin

--- apps/tutoriais_cyber_sec/admin.py
+++ apps/tutoriais_cyber_sec/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+from .models import Tutorial
 
-# Register your models here.
+
+class ListaTutoriais(admin.ModelAdmin):
+    list_display = ('id', 'titulo', 'titulo_para_url', 'data_publicacao', 'publicado')
+    list_display_links = ('id', 'titulo')
+    list_editable = ('publicado', )
+    search_fields = ('titulo_para_url', 'titulo')
+    list_filter = ('categoria',)
+    list_per_page = 10
+
+
+admin.site.register(Tutorial, ListaTutoriais)
