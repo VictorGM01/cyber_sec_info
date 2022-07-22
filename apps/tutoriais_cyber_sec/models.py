eeb64db271c0f140d2ac78366aa104c1c172d7de
Victor G. Marques <victormarques8801@gmail.com>
2022-07-22 17:53:53 -0300

    Criação das opções para as categorias dos tutoriais

--- apps/tutoriais_cyber_sec/models.py
+++ apps/tutoriais_cyber_sec/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+
+# Opções para as categorias dos tutoriais
+class Categoria(models.TextChoices):
+    CELULARES = 'Celulares'
+    COMPUTADORES = 'Computadores'
+    NAVEGADORES = 'Navegadores'
+    REDES_SOCIAIS = 'Redes Sociais'
