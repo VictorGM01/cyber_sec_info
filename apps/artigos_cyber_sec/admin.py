6f62b1733552a1b66a677d742dba3ce11c520bb7
Victor G. Marques <victormarques8801@gmail.com>
2022-07-22 17:33:22 -0300

    Personalização da tabela Artigos no admin

--- apps/artigos_cyber_sec/admin.py
+++ apps/artigos_cyber_sec/admin.py
@@ -3,10 +3,11 @@ from .models import Artigo
 
 
 class ListaArtigos(admin.ModelAdmin):
-    list_display = ('id', 'titulo_para_url', 'data_publicacao', 'publicado')
-    list_display_links = ('id', 'titulo_para_url')
+    list_display = ('id', 'titulo', 'titulo_para_url', 'data_publicacao', 'publicado')
+    list_display_links = ('id', 'titulo')
     list_editable = ('publicado', )
     search_fields = ('titulo_para_url', 'titulo')
+    list_filter = ('tema',)
     list_per_page = 10
 
 
