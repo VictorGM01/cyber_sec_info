9c47b5db426cc36abc4cba8cd54ce00bcc02e105
Victor G. Marques <victormarques8801@gmail.com>
2022-07-22 18:09:03 -0300

    Criação do modelo no bd para os Tutoriais

--- apps/tutoriais_cyber_sec/models.py
+++ apps/tutoriais_cyber_sec/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from datetime import datetime
 
 
 # Opções para as categorias dos tutoriais
@@ -7,3 +8,18 @@ class Categoria(models.TextChoices):
     COMPUTADORES = 'Computadores'
     NAVEGADORES = 'Navegadores'
     REDES_SOCIAIS = 'Redes Sociais'
+
+
+# Modelo para os tutoriais
+class Tutorial(models.Model):
+    titulo = models.CharField(max_length=100)
+    titulo_para_url = models.CharField(max_length=100)
+    capa = models.ImageField(upload_to='tutoriais/%Y/%m/%d/')
+    data_publicacao = models.DateField(default=datetime.today)
+    tempo_de_leitura = models.IntegerField()
+    tempo_de_video = models.IntegerField(blank=True)
+    motivacao = models.TextField()
+    url_video = models.URLField(blank=True)
+    conteudo = models.TextField()
+    resumo = models.TextField()
+    publicado = models.BooleanField(default=False)
