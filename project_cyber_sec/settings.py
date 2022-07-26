a8a26867c0607fd4482c078ca2d3e9f022fbb450
Victor G. Marques <victormarques8801@gmail.com>
2022-07-26 13:00:24 -0300

    Adição de novo host permitido

--- project_cyber_sec/settings.py
+++ project_cyber_sec/settings.py
@@ -32,7 +32,8 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["https://cyber-sec-info.herokuapp.com/", "127.0.0.1/", "localhost", "www.ciberseguranca.info"]
+ALLOWED_HOSTS = ["https://cyber-sec-info.herokuapp.com/", "127.0.0.1/", "localhost", "www.ciberseguranca.info",
+                 "ciberseguranca.info"]
 
 SECURE_SSL_REDIRECT = True
 
