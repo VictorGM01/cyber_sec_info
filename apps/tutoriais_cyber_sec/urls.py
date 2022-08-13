8532d9939fe12c0e77f7696437a1b000eb4abcdb
Victor G. Marques <victormarques8801@gmail.com>
2022-08-13 17:51:58 -0300

    Criação da View e url para página de tutorial

--- apps/tutoriais_cyber_sec/urls.py
+++ apps/tutoriais_cyber_sec/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.tutoriais, name='tutoriais'),
+    path('<str:nome_tutorial>', views.exibe_tutorial, name='tutorial'),
 ]
--- apps/tutoriais_cyber_sec/views.py
+++ apps/tutoriais_cyber_sec/views.py
@@ -1,5 +1,20 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404, redirect
+from .models import Tutorial
 
 
 def tutoriais(request):
     return render(request, 'tutoriais/geral_tutoriais.html')
+
+
+def exibe_tutorial(request, nome_tutorial: str):
+    tutorial_a_ser_exibido = get_object_or_404(Tutorial, titulo_para_url=nome_tutorial)
+
+    if not tutorial_a_ser_exibido.publicado:
+        return redirect('tutoriais')
+
+    conteudo = {
+        'tutorial': tutorial_a_ser_exibido,
+        'request': request,
+    }
+
+    return render(request, 'tutoriais/tutorial.html', context=conteudo)
