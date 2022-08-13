1f7a8948cf58cb7be7e09510ba02b67b15392d8f
Victor G. Marques <victormarques8801@gmail.com>
2022-08-13 18:27:27 -0300

    Adição dos artigos relacionados

--- apps/tutoriais_cyber_sec/views.py
+++ apps/tutoriais_cyber_sec/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, get_object_or_404, redirect
 from .models import Tutorial
+from apps.artigos_cyber_sec.models import Artigo
 
 
 def tutoriais(request):
@@ -15,10 +16,13 @@ def exibe_tutorial(request, nome_tutorial: str):
     tutoriais_relacionados = Tutorial.objects.order_by('-data_publicacao').filter(publicado=True).filter(
         categoria=tutorial_a_ser_exibido.categoria).exclude(titulo_para_url=nome_tutorial)[:2]
 
+    artigos_relacionados = Artigo.objects.filter(conteudo__icontains=tutorial_a_ser_exibido.titulo).filter(publicado=True)
+
     conteudo = {
         'tutorial': tutorial_a_ser_exibido,
         'request': request,
         'tutoriais_relacionados': tutoriais_relacionados,
+        'artigos_relacionados': artigos_relacionados,
     }
 
     return render(request, 'tutoriais/tutorial.html', context=conteudo)
