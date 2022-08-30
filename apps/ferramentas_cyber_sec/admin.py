e3e5435b4ad972e608ad64a6776d868ccd951c9f
Victor G. Marques <victormarques8801@gmail.com>
2022-08-30 14:42:42 -0300

    Personalização do modelo no admin

--- apps/ferramentas_cyber_sec/admin.py
+++ apps/ferramentas_cyber_sec/admin.py
@@ -1,5 +1,14 @@
 from django.contrib import admin
 from .models import Ferramenta
 
+
+class ListaFerramenta(admin.ModelAdmin):
+    list_display = ('id', 'nome', 'link', 'data_criacao')
+    list_display_links = ('id', 'nome')
+    list_editable = ('data_criacao', )
+    search_fields = ('nome', )
+    list_per_page = 10
+
+
 # Register your models here.
-admin.site.register(Ferramenta)
+admin.site.register(Ferramenta, ListaFerramenta)
