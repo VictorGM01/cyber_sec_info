f20b9f14f1063b7dc8ac92653d92768cea586b3a
Victor G. Marques <victormarques8801@gmail.com>
2022-08-29 23:01:14 -0300

    Adição da view e url da página do gerador de senhas

--- apps/ferramentas_cyber_sec/urls.py
+++ apps/ferramentas_cyber_sec/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 
 urlpatterns = [
-    path('nosso-app', views.nosso_app, name='nosso-app')
+    path('nosso-app', views.nosso_app, name='nosso-app'),
+    path('gerador-de-senhas', views.gerador_de_senhas, name='gerador-de-senhas')
 ]
--- apps/ferramentas_cyber_sec/views.py
+++ apps/ferramentas_cyber_sec/views.py
@@ -3,3 +3,7 @@ from django.shortcuts import render
 
 def nosso_app(request):
     return render(request, 'ferramentas/nosso_app.html')
+
+
+def gerador_de_senhas(request):
+    return render(request, 'ferramentas/gerador_de_senhas.html')
